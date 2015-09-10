23c121d10b06d3260e339a1c16245dbe3c9ce5c6
Dan Klco <dan.klco@sixdimensions.com>
2015-09-10 12:50:51 -0400

    Initial content

---
+++ project/src/main/java/com/sixd/coe/testt/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.sixd.coe.testt;
+
+import aQute.bnd.annotation.Version;
---
+++ project/src/test/java/com/sixd/coe/testt/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.sixd.coe.testt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
